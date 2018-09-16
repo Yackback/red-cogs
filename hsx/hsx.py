230641251432f1530f962fe7d3d4f444502516b9
Kelvin Porter <kporter@protonmail.com>
2018-09-16 00:12:23 -0400

    [hsx] add configuration, streamline commands, make stuff work

--- hsx/hsx.py
+++ hsx/hsx.py
@@ -9,19 +9,17 @@ import time
 
 import bs4
 import discord
-from discord.ext import commands
 import requests
 
-from redbot.core import checks
+from redbot.core import checks, commands
 from redbot.core.utils.chat_formatting import warning
 from redbot.core import Config
 
 dflt_guild = {"runPosttrack": True,
-              "allowed_channel": 472606694153912332,
+              "allowed_channel": None,
               "topics": list(),
-              "whitelist": list()} # TODO
-
-              # 476103105512079360 for production
+              "whitelist": list() # TODO
+}
 logging.basicConfig(
     level=logging.INFO,
     format='[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s'
@@ -36,6 +34,7 @@ class Topic(object):
         self.subject, self.body, self.tags, self.author_nick,\
             self.author, self.author_icon, self.time_ = self.resolve()
 
+
     def resolve(self):
         # Gotta scrape em all!
         r = requests.get(self.href)
@@ -46,11 +45,12 @@ class Topic(object):
                                                       subj)]
         body = resolve_soup.find("div", attrs={"id": "post_message_body"})
         body = ' '.join([p.text.lstrip() for p in body.find_all("p")])
+        def makeUpper(match):
+            return "[\[" + match.group(1).upper() + "\]](http:\/\/www\.hsx\.com\/security\/view\/" +\
+                match.group(1).upper() + ")"
         body = re.sub(r"\[([^]]+)\]",
-                      r"\[\1\](http:\/\/www\.hsx\.com\/security\/view\/\1\)",
+                      makeUpper,
                       body)
-        matches = re.finditer(r"\[([^]]]+)\]", body)
-        tags = set(tags)
         author_nick_string = resolve_soup.find("p",
                                                class_="author").text.lstrip()
         author_nick_string = author_nick_string[11:]
@@ -79,8 +79,13 @@ class HSX(object):
         self.log = logging.getLogger("redbot.cogs.hsx")
 
     async def check_channel(self, ctx):
-        # These commands only work in the hsx channel.
-        return ctx.channel.id == (await self.config.guild(ctx.guild).allowed_channel())
+        # These commands only work in the hsx channel, so we check them.
+        channel_id = await self.config.guild(ctx.guild).allowed_channel()
+        # Make sure there is an allowed channel.
+        if channel_id is None:
+            await ctx.send("Allowed channel not set up. Please run [p]hsx config set allowed_channel <allowed_id>.")
+            return False
+        return ctx.channel.id == channel_id
 
     def make_embed(self, topic: Topic):
         embed = discord.Embed(color=(discord.Color.from_rgb(167, 200, 116)))
@@ -92,10 +97,18 @@ class HSX(object):
             embed.add_field(name=topic.subject, value=topic.body)
         else:
             embed.add_field(name=topic.subject, value="\u200b")
+        # Add a field for tags in the title because you cant have links in a field name,
+        # so we display title tags separately
         if len(topic.tags) > 0:
-            embed.add_field(name="Tags", value=topic.tags)
-        embed.add_field(name="Submitted on", value=topic.time_, inline=False)
+            title_tags = ""
+            for tag in topic.tags:
+                title_tags += "[\[{}\]](https:\/\/www\.hsx\.com\/security\/view\/{}), ".format(tag, tag)
+            title_tags = title_tags[:-2] # remove the last ", "
+            embed.add_field(name="Tags From Title", value=title_tags, inline=False)
+        # Submit time
+        embed.add_field(name="Submitted on", value=topic.time_)
         return embed
+
 
     @commands.group(name="hsx")
     async def hsx_main(self, ctx):
@@ -103,16 +116,81 @@ class HSX(object):
             if await self.check_channel(ctx):
                 await ctx.send_help()
 
+
+    @checks.mod_or_permissions(manage_guild=True)
+    @hsx_main.group(name="config")
+    async def hsx_config(self, ctx):
+        # DO NOT CHECK CHANNEL SINCE ONE OF THE SUBCOMMANDS
+        # IS THE CHANNEL CHANGER
+        if ctx.invoked_subcommand == None:
+            return None
+
+
+    @hsx_main.group(name="posttrack")
+    @checks.mod_or_permissions(manage_guild=True)
+    async def hsx_posttrack(self, ctx):
+        if ctx.invoked_subcommand is None or isinstance(ctx.invoked_subcommand,
+                                                        commands.Group):
+            await ctx.send_help()
+
+
+    """
+    # WIP
     @hsx_main.command(name="stock")
-    async def hsx_stock(self, ctx, stock: str):
+    async def hsx_stock(self, ctx=None, stock_or_msg: str = ""):
+        \"""Handle printing out information about stocks.\"""
         if not await self.check_channel(ctx):
             return
         if stock is None:
             await ctx.send_help()
+    """
 
-    @hsx_main.group(name="posttrack")
+
+    @hsx_config.group(name="set")
     @checks.mod_or_permissions(manage_guild=True)
-    async def hsx_posttrack(self, ctx, quiet=False):
+    async def hsx_config_set(self, ctx):
+        if ctx.invoked_subcommand is None or isinstance(ctx.invoked_subcommand,
+                                                        commands.Group):
+            await ctx.send_help()
+    
+   
+    @hsx_config.group(name="get")
+    @checks.mod_or_permissions(manage_guild=True)
+    async def hsx_config_get(self, ctx):
+        if ctx.invoked_subcommand is None or isinstance(ctx.invoked_subcommand,
+                                                        commands.Group):
+            await ctx.send_help()
+    
+
+    # begin the getters and setters for the config stuff
+    # they dont check the channel id on purpose
+    @hsx_config_set.command(name="allowed_id")
+    @checks.mod_or_permissions(manage_guild=True)
+    async def hsx_config_set_allowed_id(self, ctx, allowed_id):
+        await self.config.guild(ctx.guild).allowed_channel.set(int(allowed_id))
+
+
+    @hsx_config_get.command(name="allowed_id")
+    @checks.mod_or_permissions(manage_guild=True)
+    async def hsx_config_get_allowed_id(self, ctx):
+        await ctx.send(await self.config.guild(ctx.guild).allowed_channel())
+
+
+    @hsx_config_set.command(name="wait_time")
+    @checks.mod_or_permissions(manage_guild=True)
+    async def hsx_config_set_wait_time(self, ctx, wait_time):
+        await self.config.guild(ctx.guild).wait_time.set(int(wait_time))
+
+
+    @hsx_config_get.command(name="wait_time")
+    @checks.mod_or_permissions(manage_guild=True)
+    async def hsx_config_get_wait_time(self, ctx):
+        await ctx.send(await self.config.guild(ctx.guild).wait_time())
+
+
+    @hsx_posttrack.command(name="start")
+    @checks.mod_or_permissions(manage_guild=True)
+    async def hsx_posttrack_start(self, ctx, quiet=False):
         """
         Track new posts on hsx's movie forums and and post them as an embed.
 
@@ -121,39 +199,41 @@ class HSX(object):
         """
         if not await self.check_channel(ctx):
             return
-        if ctx.invoked_subcommand is None or isinstance(ctx.invoked_subcommand,
-                                                        commands.Group):
-            await ctx.send("Starting")
-            url = "https://www.hsx.com/forum/forum.php?id=3"
-            await self.config.guild(ctx.guild).runPosttrack.set(True)
-            self.log.info("Starting posttrack to check for new posts...")
-            while await self.config.guild(ctx.guild).runPosttrack():
-                self.log.debug("Checking for new posts...")
-                r = requests.get(url)
-                soup = bs4.BeautifulSoup(r.content, "lxml")
-                topics = soup.find_all("p", class_="indent0 topic")
-                for topic in topics:
-                    # topic_ = instance of Topic class
-                    # topic_uniq = dict of important stuff
-                    # topic = the value of the loop
-                    current_len = len(await self.config.guild(ctx.guild).topics())
-                    # We only want to store the unique identifier, that way we
-                    # can save much more space.
-                    topic_ = Topic(topic)
-                    topic_uniq = {"subj": topic_.subject, "time": topic_.time_}
-                    async with self.config.guild(ctx.guild).topics() as current_topics:
-                        if topic_uniq not in current_topics:
-                            current_topics.append(topic_uniq)
-                    if len(await self.config.guild(ctx.guild).topics()) != current_len:
-                        # Now, when making the embed we do the full class again
-                        if not quiet:
-                            self.log.debug("New post found, sending embed.")
-                            embed = self.make_embed(topic_)
-                            await ctx.send(embed=embed)
-                if quiet:
-                    return
-                self.log.debug("Waiting 100 seconds before checking again.")
-                await asyncio.sleep(100)
+        await ctx.send("Starting")
+        url = "https://www.hsx.com/forum/forum.php?id=3"
+        await self.config.guild(ctx.guild).runPosttrack.set(True)
+        self.log.info("Starting posttrack to check for new posts...")
+        while await self.config.guild(ctx.guild).runPosttrack():
+            self.log.debug("Checking for new posts...")
+            r = requests.get(url)
+            soup = bs4.BeautifulSoup(r.content, "lxml")
+            topics = soup.find_all("p", class_="indent0 topic")
+            for topic in topics:
+                # topic_ = instance of Topic class
+                # topic_uniq = dict of important stuff
+                # topic = the value of the loop
+                current_len = len(await self.config.guild(ctx.guild).topics())
+                # We only want to store the unique identifier, that way we
+                # can save much more space.
+                topic_ = Topic(topic)
+                topic_uniq = {"subj": topic_.subject, "time": topic_.time_}
+                async with self.config.guild(ctx.guild).topics() as current_topics:
+                    if topic_uniq not in current_topics:
+                        current_topics.append(topic_uniq)
+                if len(await self.config.guild(ctx.guild).topics()) != current_len:
+                    # Now, when making the embed we do the full class again
+                    rm_str = "** This post has been removed by the forum moderator! **"
+                    if not quiet and topic_.subject != rm_str:
+                        self.log.debug("New post found, sending embed.")
+                        embed = self.make_embed(topic_)
+                        await ctx.send(embed=embed)
+            if quiet: # Quiet mode exits after population.
+                await ctx.send("Populated post cache.")
+                return
+            wait_time = self.config.guild(ctx.guild).wait_time()
+            self.log.info("Waiting {} seconds before checking again.".format(wait_time))
+            await asyncio.sleep(wait_time)
+
 
     @hsx_posttrack.command(name="set")
     async def hsx_posttrack_set(self, ctx, toggle: int):
@@ -163,10 +243,13 @@ class HSX(object):
         await self.config.guild(ctx.guild).runPosttrack.set(False if toggle == 0
                                                            else True)
 
-    @hsx_posttrack.command(name="clear")
+
     @checks.mod_or_permissions(manage_guild=True)
+    @hsx_posttrack.command(name="clear")
     async def hsx_posttrack_clear(self, ctx):
         """Clear the post cache"""
+        if not await self.check_channel(ctx):
+            return
         settings = self.config.guild(ctx.guild)
         await ctx.send(warning("WARNING: this is a dangerous operation."))
         await ctx.send("1/2: Are you sure you want to proceed? Please answer Y or N.")
@@ -192,6 +275,25 @@ class HSX(object):
         await ctx.send("Deleted post cache.")
 
         if message.content == "Y":
-            await self.hsx_posttrack(ctx, quiet=True)
+            await self.hsx_posttrack_start(ctx, quiet=True)
             await ctx.send("Quietly repopulated post cache..."
-                           "run [p]hsx posttrack to begin tracking again.")
+                           "`run [p]hsx posttrack start` to begin tracking again.")
+
+    
+            
+    """
+    # WIP
+    async def on_message(self, message):
+        settings = self.config.guild(ctx.guild)
+        if message.guild is None:
+            return
+        
+        if not await self.check_channel(await settings.allowed_channel):
+            return
+
+        matches = re.compile(r"\[([^]]+)\]").findall(message.content)
+        for match in matches:
+            await self.hsx_stock(self, stock=match[1:-1])
+        
+
+    """
